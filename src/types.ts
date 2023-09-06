992786f0c4ae8226b0e122a86327b6dcd34f3024
Ricardo da Rocha <ricardodarocha@outlook.com>
2023-09-06 00:30:11 -0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export type attributeType = "String" | "int" | "float" | "boolean" | "object" | "list" | null;
+export type delphiattributeType = "String" | "integer" | "Extended" | "boolean" | "TObject" | "TArray<" | null;
 
 export type javaAttribute = {
   name: string,
@@ -6,10 +7,21 @@ export type javaAttribute = {
   list: boolean
 };
 
+export type delphiAttribute = {
+  name: string,
+  type: delphiattributeType,
+  list: boolean
+};
+
 export type javaClass = {
   name: string,
   attributes: javaAttribute[]
 };
+export type delphiClass = {
+  name: string,
+  attributes: delphiattribute[]
+};
+
 
 export type rawDataEntity = {
   name: string,
@@ -17,6 +29,11 @@ export type rawDataEntity = {
 };
 
 export type javaClassBody = {
+  name: string,
+  textBody: string
+};
+
+export type delphiClassBody = {
   name: string,
   textBody: string
 };
